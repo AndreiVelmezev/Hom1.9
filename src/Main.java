2979802ba814b915881d226d38c2657844d8c44e
AndreiVelmezev <velmezev@gmail.com>
2022-06-20 08:43:45 +0300

    1/9 gotovo

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author author1 = new Author("Lev","Tolstoy");
+        Book book1=new Book("Voyna i mir", author1,1865);
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+
+        book1.setPublishingYear(1869);
+
+        System.out.println("Name book "+ book1.getBookName()+ " Name autor "+book1.getAuthor().getName()
+                + " "+ book1.getAuthor().getSurname()+" god publikacii "+ book1.getPublishingYear());
+
     }
 }
